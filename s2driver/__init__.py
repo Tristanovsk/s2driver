a0a29cb06393658d3c8eac70493c81ce410a020d
harmelt <tristan.harmel@get.omp.eu>
2023-09-20 11:11:00 +0200

    update for landsat 8 and 9

--- s2driver/__init__.py
+++ s2driver/__init__.py
@@ -1,4 +1,9 @@
 '''
 v1.0.2: fix for EOreader version >= 0.23
+v1.0.3: add landsat driver for L8 and L9
 '''
-__version__='1.0.2'
+
+__version__='1.0.3'
+
+from .driver_landsat_col2 import landsat_driver
+from .driver_S2_SAFE import sentinel2_driver
