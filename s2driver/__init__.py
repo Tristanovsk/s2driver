59fd722e184d20d8b38c0f9c8708593e65f15907
harmelt <tristan.harmel@get.omp.eu>
2024-01-26 12:01:41 +0100

    fix resolution discrepency beteween angle and radiometric raster

--- s2driver/__init__.py
+++ s2driver/__init__.py
@@ -1,9 +1,10 @@
 '''
 v1.0.2: fix for EOreader version >= 0.23
 v1.0.3: add landsat driver for L8 and L9
+v1.0.4: add subset option for S2
 '''
 
-__version__='1.0.3'
+__version__='1.0.4'
 
 from .driver_landsat_col2 import landsat_driver
 from .driver_S2_SAFE import sentinel2_driver
